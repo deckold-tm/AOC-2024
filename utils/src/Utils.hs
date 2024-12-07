eb8f0c754afc1ed31cdcb9c55ed88e47912a9986
David Eckold <d.eckold@techmodal.com>
2024-12-07 14:39:48 +0000

    update to utils

--- utils/src/Utils.hs
+++ utils/src/Utils.hs
@@ -1,10 +1,12 @@
-module Utils (getFileName, readFileFromArg) where
+module Utils (getFileName, readFileFromArg, runTask, AOC) where
 
-import Control.Exception (try)
+import Control.Exception (try, throw)
 import Control.Monad.Trans (MonadIO (liftIO))
-import Control.Monad.Trans.Except (ExceptT (..) )
+import Control.Monad.Trans.Except (ExceptT (..), runExceptT )
 import Control.Monad.Trans.Maybe (MaybeT (MaybeT), maybeToExceptT)
 import Data.Maybe (listToMaybe)
+import System.Environment (getArgs)
+import System.IO.Error ( isDoesNotExistError, isUserError )
 
 getFileName :: IO [String] -> ExceptT IOError IO FilePath
 getFileName args = do
@@ -20,3 +22,20 @@ readFileFromArg args = do
   path <- getFileName args
   content <- ExceptT $ do try $ readFile path
   return content
+
+type AOC = String -> IO (String, Maybe String)
+
+runTask :: AOC -> IO ()
+runTask func = do
+  content <- runExceptT $ readFileFromArg getArgs
+  case content of
+    Left err | isUserError err -> undefined
+    Left err | isDoesNotExistError err -> undefined
+    Left err -> throw err
+    Right c -> do
+      (ans1, ans2) <- func c
+      putStrLn "The answer to part 1 is:"
+      putStrLn ans1
+      case ans2 of
+        Just ans-> putStrLn $ "The answer to part 2 is:\n" ++ ans
+        Nothing -> return ()
