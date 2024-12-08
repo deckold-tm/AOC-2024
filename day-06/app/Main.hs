befc3abcdc59567fcae14feb94fb2e16d2ec07d0
David Eckold <d.eckold@techmodal.com>
2024-12-08 15:02:53 +0000

    update day6

--- day-06/app/Main.hs
+++ day-06/app/Main.hs
@@ -3,12 +3,11 @@ module Main where
 import Control.Parallel.Strategies (parMap, rdeepseq)
 import Control.Monad.Trans.State.Strict
 import Data.Array
-import Data.List ( findIndices , elemIndex)
+import Data.List ( findIndices , elemIndex, )
 import Data.Maybe (fromJust)
 import Text.Parsec (ParseError, char, many1, newline, oneOf, optional, parse, (<|>), )
 import Text.Parsec.String
 import Utils
-import Data.HashSet (member, empty, insert, HashSet)
 
 type Bounds = ((Int, Int), (Int, Int))
 
@@ -36,7 +35,7 @@ parseMap b a = return $ listArray b a
 sumObst :: Array i MapElement -> String
 sumObst a = show $ length $ filter id $ map (== Obst) $ elems a
 sumVisited :: Array i MapElement -> String
-sumVisited a = show $ (+1) $ length $ filter id $ map (== Empty True) $ elems a
+sumVisited a = show $ (+1) $ length $ filter id $ map isVisited $ elems a
 
 mapSize :: (Monad m) => String -> m Bounds
 mapSize content = do
@@ -45,9 +44,21 @@ mapSize content = do
   let width = length $ head ls
   return ((1, 1), (width, height))
 
-data MapElement = Obst | Guard {orientation :: Orientation} | Empty {isVisted :: Bool}
+data MapElement = Obst | Guard {orientation :: Orientation} | Empty {status ::  EmptyState}
 
 data Orientation = North | East | South | West deriving (Eq, Enum, Bounded)
+
+-- instance Hashable Orientation where
+--   hashWithSalt = hashUsing fromEnum
+
+data EmptyState = UnVisited | North' | East' | South' | West'
+
+updateEmptyState :: Orientation -> EmptyState
+updateEmptyState a = case a of
+  North -> North'
+  East -> East'
+  South -> South'
+  West -> West'
 
 next :: Orientation -> Orientation
 next a = if maxBound == a then minBound else succ a
@@ -56,15 +67,29 @@ type Map = Array ((Int, Int), (Int, Int)) MapElement
 
 instance Eq MapElement where
   Obst == Obst = True
-  Empty False == Empty False = True
-  Empty True == Empty True = True
+  -- Empty  == Empty b = 
+    -- if (size a == 0) && (size b == 0) then True
+    -- else if (size a >=1) && (size b >= 1) then True else False
+  Empty UnVisited == Empty UnVisited = True
+  Empty  _ == Empty  _ = True
   Guard _ == Guard _ = True
   _ == _ = False
 
+isVisited :: MapElement -> Bool
+isVisited a = case a of
+  Empty UnVisited -> False
+  Empty _ -> True
+  _ -> False
+
+isEmpty :: MapElement -> Bool
+isEmpty a =case a of
+  Empty _ -> True
+  _ -> False
+
 instance Show MapElement where
   show Obst = "#"
-  show (Empty False) = "."
-  show (Empty True) = "X"
+  show (Empty UnVisited) = "."
+  show (Empty _) = "X"
   show (Guard North) = "^"
   show (Guard East) = ">"
   show (Guard South) = "v"
@@ -74,7 +99,7 @@ parseMapCharacter :: String -> Either ParseError [MapElement]
 parseMapCharacter = parse (many1 (parseEmpty <|> parseObstruction <|> parseGuard)) "Parse Map"
 
 parseEmpty :: Parser MapElement
-parseEmpty = Empty False <$ char '.' <* optional newline
+parseEmpty = Empty UnVisited <$ char '.' <* optional newline
 
 parseObstruction :: Parser MapElement
 parseObstruction = Obst <$ char '#' <* optional newline
@@ -130,33 +155,70 @@ moveOrTurn cur = do
   return $ case a of
     Obst -> cur // [let (idx, o) = curGuard cur in (idx, Guard $ next o)]
     Empty _ ->
-      let (idx, o) = curGuard cur
-       in cur // [(idx, Empty True), (nextIdx, Guard o)]
+      let 
+        (idx, o) = curGuard cur
+        -- initHash = (insert o (empty )):: HashSet Orientation
+        -- addedHash = insert o b
+        in cur // [(idx, Empty (updateEmptyState o ::EmptyState)), (nextIdx, Guard o)]
+    -- Empty a -> undefined
     Guard _ -> undefined
 
-checkLoop :: (HashSet String, Array2d MapElement) ->  Bool
-checkLoop = evalState moveGuardLoop
+checkLoop :: (Array2d MapElement) ->  Bool
+checkLoop = evalState moveGuardLoop2
 
 tryAll :: Array2d MapElement -> [Bool]
 tryAll cur = let
-  vals = findIndices (\v -> (==Empty False) v || (== Empty True) v) $ elems cur
+  vals = findIndices (\v -> (isEmpty) v || (isEmpty) v) $ elems cur
   idxs = indices cur 
   initMap = [cur // [(idxs !! i, Obst)] | i <- vals ]
-  in parMap rdeepseq checkLoop $ zip (repeat (empty:: HashSet String) ) initMap
+  in parMap rdeepseq checkLoop $ initMap
 
 countLoops :: [Bool] -> Int
 countLoops i = length $ filter id i
 
-moveGuardLoop :: (Monad m) => StateT (HashSet String,Array2d MapElement) m Bool
-moveGuardLoop = do
-  (prev, cur) <- get
+-- moveGuardLoop :: (Monad m) => StateT (HashSet String,Array2d MapElement) m Bool
+-- moveGuardLoop = do
+--   (prev, cur) <- get
+--   let nextPlace = moveOrTurn cur
+--   let stateString = show $ fmap elems nextPlace
+--   if member stateString prev then
+--     case nextPlace of
+--       Nothing -> return False
+--       Just n -> do
+--         (s, m) <- return $ runState moveGuardLoop (insert stateString prev, n)
+--         put m
+--         return s
+--     else  return True
+
+moveGuardLoop2 :: (Monad m) => StateT (Array2d MapElement) m Bool
+moveGuardLoop2 = do
+  cur <- get
+  -- let nextPlace = moveOrTurn cur
+  let a =  checkAhead cur
   let nextPlace = moveOrTurn cur
-  let stateString = show $ fmap elems nextPlace
-  if member stateString prev then
-    case nextPlace of
-      Nothing -> return False
-      Just n -> do
-        (s, m) <- return $ runState moveGuardLoop (insert stateString prev, n)
+  let (_, go) = curGuard cur
+  case a of
+    Nothing -> return False
+    Just e -> case e of
+      Obst -> do
+        (s, m) <- return $ runState moveGuardLoop2 $ fromJust nextPlace
         put m
         return s
-    else return True
+      -- Empty UnVisited -> do
+      --   (s, m) <- return $ runState moveGuardLoop2 cur
+      --   put m
+      --   return s
+      Empty eo -> 
+        if sameDirection go eo then return True
+        else do
+          (s, m) <- return $ runState moveGuardLoop2 $ fromJust nextPlace
+          put m
+          return s
+      _ -> undefined
+
+sameDirection :: Orientation -> EmptyState -> Bool
+sameDirection North North' = True
+sameDirection South South' = True
+sameDirection East East' = True
+sameDirection West West' = True
+sameDirection _ _ = False
