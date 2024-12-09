9b83420c7d5b62d7049e2ef64ee978048d05cebc
David E <dave.eckold@gmail.com>
2024-12-09 14:54:51 +0000

    update to day-07

--- day-07/app/Main.hs
+++ day-07/app/Main.hs
@@ -6,7 +6,7 @@ main :: IO ()
 main = Utils.runTask run
 
 run :: AOC
-run content = return $ (content , Nothing)
+run content = return (show $ Main.readFile content , Nothing)
 
 data Comp = Comp {answer :: Int, inputs :: [Int]} deriving (Show)
 
@@ -15,4 +15,12 @@ parseTask = undefined
 parseAns :: Parser Int
 parseAns = (read :: String -> Int) <$> manyTill digit (char ':') <* char ' ' 
 parseInputs :: Parser [Int]
-parseInputs = map.(read :: String -> Int) <$> manyTill ((many1 digit) <* char ' ') newline
+parseInputs = map read <$> manyTill (many1 digit <* char ' ') newline
+
+parseLine :: Parser Comp
+parseLine = Comp <$> parseAns <*> parseInputs
+
+parseFile :: Parser [Comp]
+parseFile = many1 parseLine <* eof
+
+readFile = parse parseFile "Parsing File" 
