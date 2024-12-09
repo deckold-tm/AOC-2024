37b033e23ff071363fae50a8c086ee2c654ae0cc
David E <dave.eckold@gmail.com>
2024-12-09 14:54:18 +0000

    update to day6

--- day-06/app/Main.hs
+++ day-06/app/Main.hs
@@ -3,11 +3,12 @@ module Main where
 import Control.Parallel.Strategies (parMap, rdeepseq)
 import Control.Monad.Trans.State.Strict
 import Data.Array
-import Data.List ( findIndices , elemIndex, )
+import Data.List ( findIndices , elemIndex, findIndex, )
 import Data.Maybe (fromJust)
 import Text.Parsec (ParseError, char, many1, newline, oneOf, optional, parse, (<|>), )
 import Text.Parsec.String
 import Utils
+import Debug.Trace
 
 type Bounds = ((Int, Int), (Int, Int))
 
@@ -25,8 +26,9 @@ myTask content = do
     Left _ ->  undefined 
   parsedMap <- parseMap size mapList'
   return (
+    -- (trace $ showMap $ runGuard parsedMap) $ sumVisited $ runGuard parsedMap, 
     sumVisited $ runGuard parsedMap, 
-    Just $ show $ countLoops $ tryAll parsedMap
+    Just $ show $ countLoops $ tryVisited parsedMap
     )
 
 parseMap :: (Monad m) => Bounds -> [a] -> m (Array2d a)
@@ -35,7 +37,7 @@ parseMap b a = return $ listArray b a
 sumObst :: Array i MapElement -> String
 sumObst a = show $ length $ filter id $ map (== Obst) $ elems a
 sumVisited :: Array i MapElement -> String
-sumVisited a = show $ (+1) $ length $ filter id $ map isVisited $ elems a
+sumVisited a = show $ length $ filter id $ map isVisited $ elems a
 
 mapSize :: (Monad m) => String -> m Bounds
 mapSize content = do
@@ -44,52 +46,65 @@ mapSize content = do
   let width = length $ head ls
   return ((1, 1), (width, height))
 
-data MapElement = Obst | Guard {orientation :: Orientation} | Empty {status ::  EmptyState}
+-- data MapElement = Obst | Guard {orientation :: Orientation} | Empty EmptyState
 
 data Orientation = North | East | South | West deriving (Eq, Enum, Bounded)
 
--- instance Hashable Orientation where
---   hashWithSalt = hashUsing fromEnum
+data MapElement = Obst | Guard {orientation :: Orientation} | Empty {status :: VisitedState, prevOrientations :: [Orientation]}
 
-data EmptyState = UnVisited | North' | East' | South' | West'
+data VisitedState = UnVisited | Visited
 
-updateEmptyState :: Orientation -> EmptyState
-updateEmptyState a = case a of
-  North -> North'
-  East -> East'
-  South -> South'
-  West -> West'
+-- data EmptyState = UnVisited | North' | East' | South' | West'
+
+updateEmpty :: [Orientation] -> Orientation -> MapElement
+updateEmpty cur dir = Empty Visited (dir:cur)
+-- updateEmpty :: MapElement -> Orientation -> MapElement
+-- updateEmpty cur dir = case cur of
+--     Empty UnVisited _ -> Empty Visited [dir]
+--     Empty Visited lst -> Empty Visited (dir:lst)
+--     _ -> undefined
+
+
+-- updateEmptyState :: Orientation -> EmptyState
+-- updateEmptyState a = case a of
+--   North -> North'
+--   East -> East'
+--   South -> South'
+--   West -> West'
 
 next :: Orientation -> Orientation
 next a = if maxBound == a then minBound else succ a
 
 type Map = Array ((Int, Int), (Int, Int)) MapElement
 
+showMap :: (Show a) => Array2d a -> String
+showMap m = let (l,u) = bounds m in unlines [unwords [show  $ m ! (y, x) | x<- [(snd l)..(snd u)]] | y <- [(fst l)..(fst u)]]
+
+
 instance Eq MapElement where
   Obst == Obst = True
-  -- Empty  == Empty b = 
-    -- if (size a == 0) && (size b == 0) then True
-    -- else if (size a >=1) && (size b >= 1) then True else False
-  Empty UnVisited == Empty UnVisited = True
-  Empty  _ == Empty  _ = True
+  Empty UnVisited _ == Empty UnVisited _ = True
+  Empty _ _ == Empty _ _ = True
   Guard _ == Guard _ = True
   _ == _ = False
 
 isVisited :: MapElement -> Bool
 isVisited a = case a of
-  Empty UnVisited -> False
-  Empty _ -> True
+  Empty UnVisited _ -> False
+  Empty Visited _ -> True
+  Guard _ -> True
   _ -> False
 
 isEmpty :: MapElement -> Bool
 isEmpty a =case a of
-  Empty _ -> True
+  Empty _ _ -> True
   _ -> False
+
 
 instance Show MapElement where
   show Obst = "#"
-  show (Empty UnVisited) = "."
-  show (Empty _) = "X"
+  show (Empty UnVisited _) = "."
+  show (Empty Visited dir) = show $ length dir
   show (Guard North) = "^"
   show (Guard East) = ">"
   show (Guard South) = "v"
@@ -99,7 +114,7 @@ parseMapCharacter :: String -> Either ParseError [MapElement]
 parseMapCharacter = parse (many1 (parseEmpty <|> parseObstruction <|> parseGuard)) "Parse Map"
 
 parseEmpty :: Parser MapElement
-parseEmpty = Empty UnVisited <$ char '.' <* optional newline
+parseEmpty = Empty UnVisited [] <$ char '.' <* optional newline
 
 parseObstruction :: Parser MapElement
 parseObstruction = Obst <$ char '#' <* optional newline
@@ -115,16 +130,19 @@ mapDirection c = case c of
   '<' -> return $ Guard West
   _ -> undefined
 
-moveGuard :: (Monad m) => StateT (Array2d MapElement) m ()
-moveGuard = do
+moveGuard :: (Monad m) => [Orientation] -> StateT (Array2d MapElement) m ()
+moveGuard carry_dir = do
   cur <- get
-  let nextPlace = moveOrTurn cur
+  let nextPlace = moveOrTurn carry_dir cur
   case nextPlace of
     Nothing -> return ()
-    Just n -> put $ execState moveGuard n
+    Just n -> case fromJust $ checkAhead cur of
+      Obst -> put $ execState (moveGuard [] ) n
+      Empty _ dir -> put $ execState (moveGuard dir) n
+      _ -> undefined
 
 runGuard :: Array2d MapElement -> Array2d MapElement
-runGuard = execState  moveGuard
+runGuard = execState $ moveGuard []
 
 curGuard :: Array2d MapElement -> ((Int, Int), Orientation)
 curGuard cur =
@@ -148,77 +166,72 @@ ahead cur =
         West -> (h, w - 1)
       in if inRange (bounds cur) n then return n else Nothing 
 
-moveOrTurn :: Array2d MapElement -> Maybe (Array2d MapElement)
-moveOrTurn cur = do
+moveOrTurn :: [Orientation] -> Array2d MapElement -> Maybe (Array2d MapElement)
+moveOrTurn carry_dir cur = do
   a <- checkAhead cur
   nextIdx <- ahead cur
   return $ case a of
     Obst -> cur // [let (idx, o) = curGuard cur in (idx, Guard $ next o)]
-    Empty _ ->
+    Empty _ _ ->
       let 
         (idx, o) = curGuard cur
-        -- initHash = (insert o (empty )):: HashSet Orientation
-        -- addedHash = insert o b
-        in cur // [(idx, Empty (updateEmptyState o ::EmptyState)), (nextIdx, Guard o)]
-    -- Empty a -> undefined
+        in cur // [(idx, updateEmpty carry_dir o), (nextIdx, Guard o)]
+    -- Empty Visited dir -> undefined
     Guard _ -> undefined
 
-checkLoop :: (Array2d MapElement) ->  Bool
-checkLoop = evalState moveGuardLoop2
+checkLoop :: Array2d MapElement ->  Bool
+checkLoop =  evalState (moveGuardLoop2 [])
+
+tryVisited ::Array2d MapElement -> [Bool]
+tryVisited cur = let
+    start = fromJust $ elemIndex (Guard North) (elems cur)
+    comp = runGuard cur
+    vals = findIndices  isVisited $ elems comp
+    idxs = indices cur
+    initMap = [cur // [(idxs !! i, Obst)] | i <- filter (/=start)  vals]
+    in parMap rdeepseq checkLoop initMap
 
 tryAll :: Array2d MapElement -> [Bool]
 tryAll cur = let
-  vals = findIndices (\v -> (isEmpty) v || (isEmpty) v) $ elems cur
+  vals = findIndices isEmpty  $ elems cur
   idxs = indices cur 
   initMap = [cur // [(idxs !! i, Obst)] | i <- vals ]
-  in parMap rdeepseq checkLoop $ initMap
+  in parMap rdeepseq checkLoop initMap
 
 countLoops :: [Bool] -> Int
 countLoops i = length $ filter id i
 
--- moveGuardLoop :: (Monad m) => StateT (HashSet String,Array2d MapElement) m Bool
--- moveGuardLoop = do
---   (prev, cur) <- get
---   let nextPlace = moveOrTurn cur
---   let stateString = show $ fmap elems nextPlace
---   if member stateString prev then
---     case nextPlace of
---       Nothing -> return False
---       Just n -> do
---         (s, m) <- return $ runState moveGuardLoop (insert stateString prev, n)
---         put m
---         return s
---     else  return True
-
-moveGuardLoop2 :: (Monad m) => StateT (Array2d MapElement) m Bool
-moveGuardLoop2 = do
+moveGuardLoop2 :: (Monad m) => [Orientation] -> StateT (Array2d MapElement) m Bool
+moveGuardLoop2 carry_state = do
   cur <- get
-  -- let nextPlace = moveOrTurn cur
   let a =  checkAhead cur
-  let nextPlace = moveOrTurn cur
-  let (_, go) = curGuard cur
+  let nextPlace = moveOrTurn carry_state cur
+  let (_, guard_dir) = curGuard cur
   case a of
     Nothing -> return False
     Just e -> case e of
       Obst -> do
-        (s, m) <- return $ runState moveGuardLoop2 $ fromJust nextPlace
+        (s, m) <- return $ runState (moveGuardLoop2 []) (fromJust nextPlace) 
         put m
         return s
-      -- Empty UnVisited -> do
-      --   (s, m) <- return $ runState moveGuardLoop2 cur
-      --   put m
-      --   return s
-      Empty eo -> 
-        if sameDirection go eo then return True
+      Empty UnVisited dir -> do
+        (s, m) <- return $ runState (moveGuardLoop2 dir) (fromJust nextPlace) 
+        put m
+        return s
+      Empty Visited dir ->
+        -- if guard_dir `elem` dir then (trace $ showMap cur) $ return True
+        if guard_dir `elem` dir then  return True
+      -- Empty _ dir -> 
+      --   if sameDirection go dir then (trace $ show $ elems cur) $ return True
         else do
-          (s, m) <- return $ runState moveGuardLoop2 $ fromJust nextPlace
+          (s, m) <- return $ runState (moveGuardLoop2 (dir++carry_state)) $ fromJust nextPlace
           put m
           return s
       _ -> undefined
 
-sameDirection :: Orientation -> EmptyState -> Bool
-sameDirection North North' = True
-sameDirection South South' = True
-sameDirection East East' = True
-sameDirection West West' = True
-sameDirection _ _ = False
+-- sameDirection :: Orientation -> EmptyState -> Bool
+-- sameDirection North North' = True
+-- sameDirection South South' = True
+-- sameDirection East East' = True
+-- sameDirection West West' = True
+-- sameDirection _ _ = False
